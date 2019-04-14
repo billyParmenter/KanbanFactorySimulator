3857ac3610a8b67ac6b41cd4540f323525916058
azalac <zalac.austin@gmail.com>
2019-04-14 16:32:23 -0400

    Updated main function to use new logger class

--- Kanban/KanbanRunner/KanbanRunner/Program.cs
+++ Kanban/KanbanRunner/KanbanRunner/Program.cs
@@ -20,13 +20,36 @@ namespace KanbanRunner
     {
         static void Main(string[] args)
         {
-            Database db = new Database();
+            Logger logger = new Logger("KANBAN-RUNNER");
+            
+            // The using(...) & extending IDisposable will call Dispose() on db
+            // automatically when the scope is exited (on error, or on success)
+            using (Database db = new Database()) {
 
-            while (true)
-            {
-                db.Run();
-                db.Wait();
-                db.FillBins();
+                // if the database isn't connected, print the error and exit
+                if(!db.IsConnected)
+                {
+                    logger.Log("Could not connect to database:");
+                    logger.Log(db.LastError.Message);
+                    return;
+                }
+
+                logger.Log("Connected to database");
+                logger.Log("Time Scale: {0}", db.TimeScale);
+
+                // Update the current run, wait the correct amount of time, and
+                // update the bins
+                while (true)
+                {
+                    db.Run();
+                    logger.Log("Updated last run, waiting for next run");
+
+                    db.Wait();
+                    logger.Log("Done waiting, refilling bins");
+
+                    db.FillBins();
+                    logger.Log("Done filling bins");
+                }
             }
         }
     }
