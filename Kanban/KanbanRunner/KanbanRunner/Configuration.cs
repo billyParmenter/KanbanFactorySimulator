1dc1b8b5ee50593a4887f1ef125dbbd76bde047e
azalac <zalac.austin@gmail.com>
2019-04-14 16:30:57 -0400

    Moved configuration reading logic to separate class and added logger

---
+++ Kanban/KanbanRunner/KanbanRunner/Configuration.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanbanRunner
+{
+    /*
+     * Gets data from the config table
+     */
+    class Configuration
+    {
+        private readonly SqlConnection conn;
+
+        public Configuration(SqlConnection conn)
+        {
+            this.conn = conn;
+        }
+
+        /*
+         * DESCRIPTION:
+         *      Gets a double from the config table
+         * PARAMETERS:
+         *      key - The entry's name/key
+         *      result - The out-value to store the result in
+         * RETURNS:
+         *      true if the key was found and is a valid double, false otherwise
+         */
+        public bool GetDouble(string key, out double result)
+        {
+            if (!GetString(key, out string value))
+            {
+                result = 0;
+                return false;
+            }
+
+            return double.TryParse(value, out result);
+        }
+
+        /*
+         * DESCRIPTION:
+         *      Gets an int from the config table
+         * PARAMETERS:
+         *      key - The entry's name/key
+         *      result - The out-value to store the result in
+         * RETURNS:
+         *      true if the key was found and is a valid int, false otherwise
+         */
+        public bool GetInteger(string key, out int result)
+        {
+            if(!GetString(key, out string value))
+            {
+                result = 0;
+                return false;
+            }
+
+            return int.TryParse(value, out result);
+        }
+
+        /*
+         * DESCRIPTION:
+         *      Gets a value from the config table
+         * PARAMETERS:
+         *      key - The entry's name/key
+         *      result - The out-value to store the result in
+         * RETURNS:
+         *      true if the key was found (is always valid, values stored as strings)
+         */
+        public bool GetString(string key, out string result)
+        {
+            using (SqlCommand cmd = new SqlCommand("SELECT [Value] FROM CONFIG WHERE [Key] = @key", conn))
+            {
+                SqlParameter param = new SqlParameter("key", System.Data.SqlDbType.VarChar, key.Length)
+                {
+                    Value = key
+                };
+                cmd.Parameters.Add(param);
+
+                result = cmd.ExecuteScalar().ToString();
+                return result != null;
+            }
+        }
+        
+    }
+}
---
+++ Kanban/KanbanRunner/KanbanRunner/Logger.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanbanRunner
+{
+    /*
+     * A helper class which logs messages to the console, with a timestamp and
+     * name
+     */
+    class Logger
+    {
+        private readonly string name;
+
+        public Logger(string name)
+        {
+            this.name = name;
+        }
+
+        /*
+         * DECRIPTION:
+         *      Logs a message
+         * PARAMETERS:
+         *      message - The message
+         */
+        public void Log(string message)
+        {
+            Console.WriteLine(GetPrefix() + message);
+        }
+
+        /*
+         * DECRIPTION:
+         *      Logs a message with an argument
+         * PARAMETERS:
+         *      message - The message
+         *      arg - The argument (console-type formatting)
+         */
+        public void Log(string message, object arg)
+        {
+            Console.WriteLine(GetPrefix() + message, arg);
+        }
+
+        /*
+         * DECRIPTION:
+         *      Logs a message with multiple arguments
+         * PARAMETERS:
+         *      message - The message
+         *      args - The params list of arguments
+         */
+        public void Log(string message, params object[] args)
+        {
+            Console.WriteLine(GetPrefix() + message, args);
+        }
+
+        /*
+         * DESCRIPTION:
+         *      Gets the prefix for a message
+         * RETURNS:
+         *      The prefix, with format "[H:MM AM/PM <name>]: "
+         */
+        private string GetPrefix()
+        {
+            return string.Format("[{0} {1}]: ", DateTime.Now.ToShortTimeString(), name);
+        }
+    }
+}
