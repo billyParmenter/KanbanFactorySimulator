cf87d7fea1bae8ef9cac437039a76ab7ff4233da
azalac <zalac.austin@gmail.com>
2019-04-18 00:53:35 -0400

    Added some views & procedures and changed testing procedure

--- Kanban/KanbanDbSetup.sql
+++ Kanban/KanbanDbSetup.sql
@@ -24,6 +24,7 @@ insert into Config
 	([key], [value])
 Values
 	('Time_Scale', '1'),
+	('RunnerThreshold', '5'),
 	-- delay between runner checks in seconds
 	('Runner_Delay', '300'),
 	('Harness_Count', '55'),
@@ -35,10 +36,9 @@ Values
 	('Test_Tray_Cap', '60');
 
 
-
 Create Table [Event](
 	[Key] varchar(30),
-	[Value] int
+	[Value] sql_variant
 )
 
 
@@ -105,7 +105,7 @@ Insert into Experience (Time_Deviancy, Fail_rate)
 Values (1.5, 0.85);
 
 Insert into Experience (Time_Deviancy, Fail_rate)
-Values (0, 0.5);
+Values (1, 0.5);
 
 Insert into Experience (Time_Deviancy, Fail_rate)
 Values (.85, 0.15);
@@ -113,9 +113,34 @@ Values (.85, 0.15);
 go
 
 
+-- Easy way to get information about a lane's worker
+CREATE OR ALTER VIEW [LaneWorkerInfo]
+AS		SELECT Lane.LaneID, Lane.WorkerID, Experience.Time_Deviancy, Experience.Fail_Rate
+	FROM Lane
+	INNER JOIN Worker
+	ON Lane.WorkerID = Worker.WorkerID
+	INNER JOIN Experience
+	ON Experience.Experience_Level = Worker.Experience_Level
+GO
+
+
+--- An easy way to get information about a unit
+CREATE OR ALTER VIEW [UnitInfo]
+AS		SELECT Tray_Position as 'Position', Test_Tray.Test_TrayID as 'TrayID', Lane.LaneID, Worker.WorkerID,
+		Tested as 'UnitTested', Test_Tray.Part_Count as 'PartsInTray', Test_Tray.Capacity as 'TrayCapacity'
+	FROM Test_Unit
+	INNER JOIN Test_Tray
+	ON Test_Tray.Test_TrayID = Test_Unit.Test_TrayID
+	INNER JOIN Lane
+	ON Lane.LaneID = Test_Tray.LaneID
+	INNER JOIN Worker
+	ON Worker.WorkerID = Lane.WorkerID
+GO
+
+
 
 --Procedure to create a worker with the given experience
-create Procedure Create_Worker
+create or alter Procedure Create_Worker
 	@Exp int
 As
 Begin
@@ -135,8 +160,7 @@ go
 
 
 --Create a lane operated by a given worker
-go
-Create Procedure Create_Lane
+Create or alter Procedure Create_Lane
 	@laneID int,
 	@WorkerID int
 As
@@ -193,39 +217,65 @@ Create Procedure Get_WorkerInfo
 As
 	
 Begin
-	Select Lane.WorkerID, Experience.Time_Deviancy, Experience.Fail_Rate from Lane
-	Inner Join Worker on Lane.WorkerID = Worker.WorkerID 
-	Inner Join Experience on Worker.Experience_Level = Experience.Experience_Level
-	Where LaneID = @LaneID
+	Select WorkerID, Time_Deviancy, Fail_Rate from LaneWorkerInfo Where LaneID = @LaneID
 End
 Go
 
---test the product
-create Procedure Test_Product
-	@Fail int,
-	@workerID int
-As
-	Declare @Tray_pos int
-	Declare @TrayID int
-Begin
-SELECT TOP 1 [Tray_Position]
-			,[Test_TrayID]
-			,[WorkerID]
-			,[Tested]
-			,[Test_Result]
-		FROM [kanban].[dbo].[Test_Unit]
-		Where WorkerID = @workerID
-		order by Test_TrayID desc, Tray_Position desc
 
-	Update Test_Unit Set Tested = 1, Test_Result = @Fail Where Test_TrayID = @TrayID and Tray_Position = @Tray_pos
+
+-- Tests all untested units in full trays for the current lane
+CREATE OR ALTER PROCEDURE Do_Tray_Test
+	@LaneID INT
+AS
+	DECLARE @TrayID INT;
+	DECLARE @Tray_Position INT;
 	
-End
-Go
+	DECLARE @FailThreshold FLOAT;
+	DECLARE @Number FLOAT;
+	DECLARE @value BIT;
+
+	DECLARE @PartsTested INT;
+BEGIN
+	-- Get all untested units which are in a full tray and owned by the requested lane
+	DECLARE UnitCursor CURSOR
+	FOR SELECT Position, TrayID FROM UnitInfo WHERE LaneID = @LaneID AND UnitTested = 0 AND PartsInTray = TrayCapacity;
+	OPEN UnitCursor;
+
+	-- Get the fail threshold (0-1) for the worker
+	SELECT @FailThreshold = (1 - Fail_Rate / 100.0) FROM LaneWorkerInfo WHERE LaneID = @LaneID;
+
+	FETCH NEXT FROM UnitCursor INTO @Tray_Position, @TrayID;
+
+	SET @PartsTested = 0;
+
+	WHILE @@FETCH_STATUS = 0
+	BEGIN
+		-- Generate a random decimal value (approximated to 5 significant digits)
+		SELECT @Number = abs(checksum(rand()) % 10000.0) / 10000.0;
+		
+		-- If this part fails, set its result to 1, otherwise set it to 0
+		IF @Number > @FailThreshold
+			UPDATE Test_Unit SET Tested = 1, Test_Result = 1 WHERE Tray_Position = @Tray_Position AND Test_TrayID = @TrayID;
+		ELSE
+			UPDATE Test_Unit SET Tested = 1, Test_Result = 0 WHERE Tray_Position = @Tray_Position AND Test_TrayID = @TrayID;
+
+		SET @PartsTested = @PartsTested + 1;
+
+		FETCH NEXT FROM UnitCursor INTO @Tray_Position, @TrayID;
+	END
+	
+	CLOSE UnitCursor;
+	DEALLOCATE UnitCursor;
+
+	SELECT @PartsTested as 'Parts Tested';
+
+END
+GO
 
 
 
 --Creates a part from the given lane, will handle full tray and gives a new tray
-Create Procedure Part_Created
+Create or Alter Procedure Part_Created
 	@LaneID int
 As
 	Declare @Position int
@@ -240,7 +290,7 @@ As
 	Declare @Bezel_Count int
 
 Begin
-
+	
 	Select @Harness_Count = Harness_Count, @Reflector_Count = Reflector_Count, @Housing_Count = Housing_Count, @Lens_Count = Lens_Count, @Bulb_Count = Bulb_Count, @Bezel_Count = Bezel_Count from lane
 	where LaneID = @LaneID
 
@@ -267,12 +317,11 @@ Begin
 			Set @Position = 1
 
 			Insert into Test_Tray (LaneID, Capacity)
-			Select @LaneID, [value]
-			From Config
-			where [key] = 'Test_Tray_Cap'
-
+				Select @LaneID, [value]
+				From Config
+				where [key] = 'Test_Tray_Cap'
+				
 			Select Top 1 @TrayID = Test_TrayID From Test_Tray Where LaneID = @LaneID Order by Test_TrayID desc
-
 
 		End
 
@@ -280,9 +329,10 @@ Begin
 
 
 		Insert into Test_Unit(Tray_Position, Test_TrayID, WorkerID)
-		Select @Position, @TrayID, Lane.WorkerID
-		From Lane
-		Where LaneID = @LaneID
+			Select @Position, @TrayID, Lane.WorkerID
+			From Lane
+			Where LaneID = @LaneID
+
 	End
 
 End
@@ -292,8 +342,8 @@ go
 
 --Check what bins need to be filled and add the tags to the event table
 Create Trigger Check_Bins
-on Test_Unit
-for Insert
+on Lane
+for Update
 As
 	Declare @Harness_Count int
 	Declare @Reflector_Count int
@@ -356,7 +406,7 @@ declare @lane int
 
 Begin
 
-	select top 1 @column = [key], @lane = [Value] from [event] order by [value]
+	select top 1 @column = [key], @lane = CONVERT(INT, [Value]) from [event] order by [value]
 	print @column
 
 	while (@column != 'Last_Run')
@@ -366,7 +416,7 @@ Begin
 
 		Delete from [event] where [key] = @column and [value] = @lane
 
-		select top 1 @column = [key], @lane = [Value] from [event] order by [value]
+		select top 1 @column = [key], @lane = CONVERT(INT, [Value]) from [event] order by [value]
 
 		print @column
 	end
@@ -378,21 +428,20 @@ go
 
 
 
-
 -- Gets the percentage finished of the current lane (range [0-1]).
 -- If the result is >1, the item is finished and another has not been queued
 CREATE PROCEDURE Get_Remaining_Time (
 	@LaneID INT
 )
 AS
-	DECLARE @TimeRemaining float
-	DECLARE @TotalTime float
+	DECLARE @TimeElapsed FLOAt
+	DECLARE @TotalTime FLOAt
 BEGIN
-	SELECT @TimeRemaining = CONVERT(float, DATEDIFF(second, [Start_Time], GETDATE())),
-		   @TotalTime = CONVERT(float, DATEDIFF(second, [Start_Time], [End_Time]))
+	SELECT @TimeElapsed = CONVERT(FLOAt, DATEDIFF(second, [Start_Time], GETDATE())),
+		   @TotalTime = CONVERT(FLOAt, DATEDIFF(second, [Start_Time], [End_Time]))
 		   FROM Lane WHERE LaneID = @LaneID
 	
-	SELECT (@TotalTime / @TimeRemaining) AS Percent_Finished
+	SELECT (@TimeElapsed / @TotalTime) AS 'Percent Finished'
 END
 GO
 
@@ -409,5 +458,66 @@ BEGIN
 		Start_Time = GETDATE(),
 		End_Time = DATEADD(second, @SecondsToComplete, GETDATE())
 		WHERE LaneID = @LaneID
+END
+GO
+
+
+
+-- Gets the time to the next runner check in percent (0-1)
+CREATE PROCEDURE Get_Runner_Time
+AS
+	DECLARE @RunnerDelay FLOAT
+	DECLARE @LastRun DATETIME
+BEGIN
+	
+	-- The length of (real) time between runner runs
+	SELECT @RunnerDelay = CONVERT(FLOAT, (SELECT [Value] FROM Config WHERE [Key] = 'Runner_Delay'))
+						/ CONVERT(FLOAT, (SELECT [Value] FROM Config WHERE [Key] = 'Time_Scale'));
+
+	-- Only one 'Last_Run' can be in the table at once
+	SELECT @LastRun = CONVERT(DATETIME, [Value]) FROM [Event] WHERE [Key] = 'Last_Run';
+
+	SELECT (CONVERT(FLOAT, DATEDIFF(second, @LastRun, GETDATE())) / @RunnerDelay) AS 'Percent To Check',
+	@RunnerDelay, @LastRun;
+END
+GO
+
+
+-- Updates the last runner time to 'right now'
+CREATE PROCEDURE Update_Runner_Time
+AS
+BEGIN
+
+	DELETE FROM [Event] WHERE [Key] = 'Last_Run';
+	INSERT INTO [Event] ([Key], [Value]) VALUES ('Last_Run', GETDATE());
+END
+GO
+
+-- Gets the good parts, bad parts, and percent yield for a given lane
+CREATE PROCEDURE Get_Lane_Statistics (
+	@LaneID INT
+)
+AS
+	DECLARE @GoodCount INT
+	DECLARE @BadCount INT
+	DECLARE @PercentYield FLOAT
+BEGIN
+	
+	SELECT @GoodCount = COUNT(*) FROM Test_Unit
+		INNER JOIN Test_Tray
+		ON Test_Unit.Test_TrayID = Test_Tray.Test_TrayID
+		WHERE Test_Tray.LaneID = @LaneID AND Test_Unit.Tested = 1 AND Test_Unit.Test_Result = 0;
+
+	SELECT @BadCount = COUNT(*) FROM Test_Unit
+		INNER JOIN Test_Tray
+		ON Test_Unit.Test_TrayID = Test_Tray.Test_TrayID
+		WHERE Test_Tray.LaneID = @LaneID AND Test_Unit.Tested = 1 AND Test_Unit.Test_Result = 1;
+
+	IF (@GoodCount + @BadCount) = 0
+		SET @PercentYield = 0;
+	ELSE
+		SET @PercentYield = (@GoodCount / CONVERT(FLOAT, @GoodCount + @BadCount));
+	
+	SELECT @GoodCount AS 'Good Parts', @BadCount AS 'Bad Parts', @PercentYield as '% Yield';
 END
 GO
